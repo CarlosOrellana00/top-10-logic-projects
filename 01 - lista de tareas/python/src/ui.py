0c424247a2ff65b7dd79648fd798eb5ad7876b6c
CarlosOrellana00 <carlosorellana.oa@gmail.com>
2026-01-01 19:56:25 -0400

    ui.py

--- 01 - lista de tareas/python/src/ui.py
+++ 01 - lista de tareas/python/src/ui.py
@@ -37,4 +37,63 @@ def imprimir_tareas(gestor: GestorTareas, filtro: str) -> None:
     print("8) Eliminar tarea")
     print("0) Salir")
 
+  def ejecutar_app()-> None:
+    data = cargar_json(RUTA_DB)
+    gestor = GestorTareas.from_json_list(data)
 
+    while True:
+      menu()
+      option = input("\nElige una opcion: ").strip()
+
+      try:
+            if opcion == "1":
+                titulo = input("Título de la tarea: ")
+                t = gestor.crear(titulo)
+                guardar_json(RUTA_DB, gestor.to_json_list())
+                print(f"\n✅ Tarea creada (ID {t.id}).\n")
+
+            elif opcion == "2":
+                imprimir_tareas(gestor, "todas")
+
+            elif opcion == "3":
+                imprimir_tareas(gestor, "pendientes")
+
+            elif opcion == "4":
+                imprimir_tareas(gestor, "completadas")
+
+            elif opcion == "5":
+                tarea_id = pedir_entero("ID de la tarea a editar: ")
+                nuevo = input("Nuevo título: ")
+                gestor.editar(tarea_id, nuevo)
+                guardar_json(RUTA_DB, gestor.to_json_list())
+                print("\n✏️  Tarea actualizada.\n")
+
+            elif opcion == "6":
+                tarea_id = pedir_entero("ID de la tarea a completar: ")
+                gestor.marcar_completada(tarea_id, True)
+                guardar_json(RUTA_DB, gestor.to_json_list())
+                print("\n✅ Marcada como completada.\n")
+
+            elif opcion == "7":
+                tarea_id = pedir_entero("ID de la tarea a dejar pendiente: ")
+                gestor.marcar_completada(tarea_id, False)
+                guardar_json(RUTA_DB, gestor.to_json_list())
+                print("\n⏳ Marcada como pendiente.\n")
+
+            elif opcion == "8":
+                tarea_id = pedir_entero("ID de la tarea a eliminar: ")
+                gestor.eliminar(tarea_id)
+                guardar_json(RUTA_DB, gestor.to_json_list())
+                print("\n🗑️  Tarea eliminada.\n")
+
+            elif opcion == "0":
+                print("\n¡Listo! Cerrando...\n")
+                break
+
+            else:
+                print("\nOpción inválida.\n")
+
+        except ValueError as e:
+            print(f"\n Alerta  {e}\n")
+        except Exception as e:
+            print(f"\n Error inesperado: {e}\n")
