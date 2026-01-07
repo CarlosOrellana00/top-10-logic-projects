9684455cb7576fd3cba0913a90479b4efb729717
CarlosOrellana00 <carlosorellana.oa@gmail.com>
2026-01-06 23:53:42 -0400

    gestor add t

--- 01 - lista de tareas/js/js/gestor_tareas.js
+++ 01 - lista de tareas/js/js/gestor_tareas.js
@@ -5,3 +5,23 @@ export function obtenerTareas(){
   return leerTareas();
 }
 
+//agregar una tarea nueva
+export function agregarTarea(titulo){
+  const tareas = leerTareas();
+
+  const texto = titulo.trim();
+  if(texto.length === 0) return null;
+
+  const nueva = {
+    id: Date.now(),
+    titulo: texto,
+    completa: false,
+    creada_en: new Date().toISOString().slice(0,10),
+  };
+
+  tareas.push(nueva);
+  guardarTareas(tareas)
+  return nueva;
+}
+
+//Alterna completada (true/false) por ID
