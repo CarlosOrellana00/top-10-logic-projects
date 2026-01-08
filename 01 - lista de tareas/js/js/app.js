a2ae5bf64b75556851414e616c9793518ab577ff
CarlosOrellana00 <carlosorellana.oa@gmail.com>
2026-01-07 22:53:09 -0400

    app v1

--- 01 - lista de tareas/js/js/app.js
+++ 01 - lista de tareas/js/js/app.js
@@ -5,3 +5,33 @@ import{
   eliminarTarea,
 } from "./gestor_tareas.js";
 
+import { limpiarTareas } from "./almacenamiento.js";
+
+// 1.- Referencias a elementos HTML (DOM)
+const formAgregar = document.getElementById("form-agregar");
+const inputTarea = document.getElementById("input-tarea");
+const listarTareas = document.getElementById("lista-tareas");
+const mensajeVacio = document.getElementById("mensaje-vacio");
+const botonesFiltro = document.querySelector(".btn-filtro");
+const btnLimpiar = document.getElementById("btn-limpiar");
+
+//2.- Estado simple (que el filtro esta vacio)
+let filtroActual = "todas";
+
+//3.- Fucncion que dibuja la lista en pantalla
+
+//4.- Evento de agregar tarea
+
+//5.- Evento de filtros
+
+//6.- Evento de limpair todo
+btnLimpiar.addEventListener("click", () =>{
+  const ok = confirm("¿Seguro que quieres borrar TODAS las tareas?");
+  if (!ok) return;
+
+  limpiarTareas();
+  renderizar();
+});
+//7.- Primea carga, marca a "todas" como activo y renderizar.
+document.querySelector('[data-filtro="todas').classList.add("activo");
+renderizar();
