7bcabb7d52be513471657a4658023d3a7471251b
CarlosOrellana00 <carlosorellana.oa@gmail.com>
2026-01-15 16:09:41 -0400

    app.js

--- 02 - calculadora/js/js/app.js
+++ 02 - calculadora/js/js/app.js
@@ -9,3 +9,25 @@ const btnLimpiar = document.getElementById("btnLimpiar");
 const salida = document.getElementById("salida");
 
 //2.- funcion leer numeros de inputs y validacion
+function leerNumero(input){
+  const texto = input.value.trim().replace(",",".");
+  const numero = Number(texto);
+
+  if(texto === "" || Number.isNaN(numero)){
+    return null;
+  }
+  return numero;
+}
+
+//3.- mostrar mensajes en pantalla
+function mostrarMensaje(texto , tipo = "info"){
+  salida.textContent = texto;
+  salida.dataset.tipo = tipo; // para CSS
+}
+
+//4.- calcular
+
+//5.- limpiar
+
+//6.- mensaje inicial
+mostrarMensaje("Ingresa numeros y selecciona una operacion.", "info");
