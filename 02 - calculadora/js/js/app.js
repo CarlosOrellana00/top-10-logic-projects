13d61592df4624f326e7cdee27e21601873a6160
CarlosOrellana00 <carlosorellana.oa@gmail.com>
2026-01-15 17:06:18 -0400

    calculador PY -JS

--- 02 - calculadora/js/js/app.js
+++ 02 - calculadora/js/js/app.js
@@ -1,4 +1,4 @@
-import { sumar, restar, multiplicar, dividir} from "./calculadora";
+import { sumar, restar, multiplicar, dividir} from "./calculadora.js";
 
 //1.- traer elementos de la vista HTML
 const inputA = document.getElementById("numeroA");
@@ -26,8 +26,37 @@ function mostrarMensaje(texto , tipo = "info"){
 }
 
 //4.- calcular
+btnCalcular.addEventListener("click", () => {
+  const a = leerNumero(inputA);
+  const b = leerNumero(inputB);
+
+  if(a === null || b === null){
+    mostrarMensaje("Ingresa dos numeros validos.", "error");
+    return;
+  }
+
+  try{
+    let resultado;
+
+    const op = selectOperacion.value;
+    if(op === "sumar") resultado = sumar(a,b);
+    else if (op === "restar") resultado = restar(a,b);
+    else if (op === "multiplicar") resultado = multiplicar(a,b);
+    else if (op === "dividir") resultado = dividir(a,b);
+
+    mostrarMensaje(`resultado: ${resultado}`, "ok");
+  }catch(error){
+    mostrarMensaje(`Error: ${error.message}`,"error");
+  }
+});
 
 //5.- limpiar
-
+btnLimpiar.addEventListener("click", () => {
+  inputA.value = "";
+  inputB.value = "";
+  selectOperacion.value = "sumar";
+  mostrarMensaje("Ingresa numeros y selecciona una operacion.", "info");
+  inputA.focus();
+});
 //6.- mensaje inicial
 mostrarMensaje("Ingresa numeros y selecciona una operacion.", "info");
