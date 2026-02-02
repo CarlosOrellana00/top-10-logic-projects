9878c57446e50a30c6fd5e519383c1ccb746f12c
CarlosOrellana00 <carlosorellana.oa@gmail.com>
2026-02-02 13:57:01 -0400

    6 clima lista

--- 06 - clima/python/main.py
+++ 06 - clima/python/main.py
@@ -26,5 +26,43 @@ class ClimaApp:
     titulo.pack(pady==(16,10))
 
     #3.- Zona de busqueda (Ciudad y País)
+    frame_busqueda = tk.Frame(self.root, bg="0b0f14")
+    frame_busqueda.pack(pady=(0,10))
+
+    tk.Label(
+      frame_busqueda, text="Ciudad",
+      font=("Consolas",12),
+      fg="#c7d0d9", bg="#0b0f14"
+    ).grid(row=0, colum=0, padx=(0,0), pady=6, sticky="e")
+
+    self.ciudad_var = tk.StringVar(value="Santiago")
+    self.entry_ciudad = tk.Entry(
+      frame_busqueda,
+      textvariable=self.ciudad_var,
+      width=22,
+      font=("Consolas",12)
+    )
+    self.entry_ciudad.grid(row=0, column=1, pady=6)
+
+    tk.Label(
+      frame_busqueda, text="Pais: ",
+      font=("Consolas",12),
+      fg="#c7d0d9", bg="#0b0f14"
+    ).grid(row=0,column=2, padx=(18,8), pady=6, sticky="e")
+
+    self.pais_var = tk.StringVar(value="Chile")
+    self.entry_ciudad = tk.Entry(
+      frame_busqueda,
+      textvariable=self.pais_var,
+      width=18,
+      font=("Consolas",12)
+    )
+    self.entry_pais.grid(row=0, column=3, pady=6)
+
+    #4.- Boton Buscar
+    #5.- Zona de resultado
+    #6.- Caja grande para pronostico
+    #7.- Atajo Enter para buscar
+
 
 
