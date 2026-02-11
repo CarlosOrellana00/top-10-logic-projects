5ec09253d70e1b81d23d5da02810bac7624778f4
CarlosOrellana00 <carlosorellana.oa@gmail.com>
2026-02-11 16:39:59 -0400

    08 js app inicio

---
+++ 08 - cuestionario/js/js/app.js
@@ -0,0 +1,40 @@
+// 1.- Capturar elementos del DOM
+const pantallaInicio = document.getElementById("pantallaInicio");
+const pantallaJuego = document.getElementById("pantallaJuego");
+const pantallaResultado = document.getElementById("pantallaResultado");
+
+const nombreInput = document.getElementById("nombre");
+const btnIniciar = document.getElementById("btnIniciar");
+const errorInicio = document.getElementById("errorInicio");
+
+const nombreJugadorEl = document.getElementById("nombreJugador");
+const progresoEl = document.getElementById("progreso");
+const puntajeEl = document.getElementById("puntaje");
+const enunciadoEl = document.getElementById("enunciado");
+const opcionesEl = document.getElementById("opciones");
+const feedbackEl = document.getElementById("feedback");
+const btnSiguiente = document.getElementById("btnSiguiente");
+
+const resumenFinalEl = document.getElementById("resumenFinal");
+const mensajeFinalEl = document.getElementById("mensajeFinal");
+const btnReiniciar = document.getElementById("btnReiniciar");
+
+// 2.- Estado del juego
+
+// 3.- Utilidades
+
+// 4.- Render de la Pregunta
+
+// 5.- Responder Pregunta
+
+// 6.- Pasar a la siguiente pregunta o terminar
+
+// 7.- Terminar a la siguiente pregunta o terminar
+
+// 8.- Iniciar Juego
+
+// 9.- Reiniciar
+
+// 10.- Eventos
+
+// Enter inicia desde el input nombre
